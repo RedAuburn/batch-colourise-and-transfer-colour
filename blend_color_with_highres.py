c1e7648a4b360ccf19ac9840278a8ea67e00d742
endi <smashedchant@gmail.com>
2020-05-10 05:33:00 +0100

    finished blend script

--- blend_color_with_highres.py
+++ blend_color_with_highres.py
@@ -6,8 +6,11 @@ import sys, os
 ftypes =[("image files","*.jpg;*.jpeg;*.png")]
 
 
+#go away tk window
+root = tk.Tk()
+root.withdraw()
 
-#get images
+#get image paths when standalone or when using parameters from another script
 def getimages(hrpathimport,cpathimport,imported):
     if(not imported):
         highrespath = tk.filedialog.askopenfilename(initialdir="/", title="Select highres image", filetypes=ftypes)
@@ -15,36 +18,40 @@ def getimages(hrpathimport,cpathimport,imported):
         return(highrespath,colourpath)
     if(imported):
         return(hrpathimport,cpathimport)
-
-def scalecoloured(highrespath,colourpath):#done
+    
+#scales the coloured image to the same size as the high res.
+def scalecoloured(highrespath,colourpath):
     himgres = Image.open(highrespath).size
     cimg = Image.open(colourpath)
     cimg = cimg.resize(himgres,Image.BILINEAR)
     cimg.save(colourpath)
-    return("finished scaling colourised img")
-
+    
+#splits the HS channels from colour image, merges it with V from the high res. (ps color blend mode)
 def colourblend(highrespath,colourpath):
     colourhs = Image.open(colourpath).convert("HSV")
     colourh = colourhs.getchannel(0)
     colours = colourhs.getchannel(1)
-    hiresv = Image.open(highrespath).convert("L") #highresluminoscity
+    hiresv = Image.open(highrespath).convert("L") 
     final = Image.merge("HSV",(colourh,colours,hiresv))
-    #final.save(colourpath)
-    final.show()
+    final = final.convert("RGB")
+    final.save(colourpath)
     
-    return("finished blending images")
 
     
 #main 
 def main(hrpathimport,cpathimport,importedtest):
     filepaths = getimages(hrpathimport,cpathimport,importedtest)
     if(filepaths[0] != "" or filepaths[1] != ""):
+        print("starting colour image scaling...")
         scalecoloured(filepaths[0],filepaths[1])
+        print("finished colour image scaling\n")
+        print("starting image blending...")
         colourblend(filepaths[0],filepaths[1])
+        print("finished image blending\n")
     else:
         print("please select two images\n")
     
-    
+#to prevent script being run on import in other scripts.  
 if __name__ == "__main__":
     main("nothing entered yet\n","",False)
     
