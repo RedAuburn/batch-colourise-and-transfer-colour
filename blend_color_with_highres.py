ef7bddb308fa5b1a2aa06f122bcf36fcc1a31f46
endi <smashedchant@gmail.com>
2020-05-10 03:54:35 +0100

    cleanup

--- blend_color_with_highres.py
+++ blend_color_with_highres.py
@@ -1,15 +1,14 @@
 from tkinter import filedialog
 import tkinter as tk
-import PIL as pl
-import sys
-import os
+import PIL
+from PIL import Image
+import sys, os
 
 ftypes =[("image files","*.jpg;*.jpeg;*.png")]
 
 
 
 #get images
-
 def getimages(hrpathimport,cpathimport,imported):
     if(not imported):
         highrespath = tk.filedialog.askopenfilename(initialdir="/", title="Select highres image", filetypes=ftypes)
@@ -18,15 +17,27 @@ def getimages(hrpathimport,cpathimport,imported):
     if(imported):
         return(hrpathimport,cpathimport)
 
+def scalecoloured(colourtoscale):
+    #need to scale the coloured image to the same resolution as original highres to overlay.
+    return("nothing here yet")
+
+def colourblend(highresin,colourin):
+    #for safety, convert highres to grayscale with this stuff
+    #img = Image.open('image.png').convert('LA')
+    #img.save('greyscale.png')
+    return("nothing here yet")
+
     
 #main 
 def main(hrpathimport,cpathimport,importedtest):
-    return getimages(hrpathimport,cpathimport,importedtest)
+    filepaths = getimages(hrpathimport,cpathimport,importedtest)
+    scalecoloured(filepaths[1]) #will apply to image so don't need to assign
+    colourblend(filepaths[0],filepaths[1]) 
+    
     
 if __name__ == "__main__":
-    print(main("","",False))
+    main("nothing entered yet","",False)
+    
 
 
     
-#colourimg = pl.Image.open(filepath)
-
