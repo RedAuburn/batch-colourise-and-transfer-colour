4e5c2e89da32c2777b8e9a2b42413b16c957573a
endi <smashedchant@gmail.com>
2020-05-10 15:20:36 +0100

    fixed key check
    will prompt you to sign up for a key if key isnt set

--- batch_colourise_with_deepai.py
+++ batch_colourise_with_deepai.py
@@ -6,31 +6,30 @@ highrescolouroverlay = True
 yourapikey = "enter api key here"
 
 
-if(not yourapikey or yourapikey="enter your api key here"):
-   print("you are using the default key, please consider signing up at deepai.org :)")
-   os.system("start \"\" https://deepai.org/dashboard/profile")
-
-newdir = os.path.join(os.getcwd(),r'colourised')
-if not os.path.exists(newdir):
-   os.makedirs(newdir)
-
-for file in os.listdir():
-    try:
-          print("using: "+file)
-          r = requests.post(
-             "https://api.deepai.org/api/colorizer",
-             files={'image': open(file, 'rb'),},
-             headers={'api-key': yourapikey}
-         )
-          outlink = r.json()
-          newfilename = os.path.splitext(os.path.basename(file))[0]+"_colourised.jpg"
-          newfilepath = os.path.join(newdir,newfilename)
-          originalfilepath = os.path.abspath(file)
-          urllib.request.urlretrieve(outlink["output_url"],newfilepath)
-          print("created new file "+newfilename+"\n")
-          if (highrescolouroverlay):
-             bcwh.main(originalfilepath,newfilepath,True)
-    except:
-        print("skipping "+file+", not valid image.\n")
+if(yourapikey and yourapikey!="enter api key here"):
+   newdir = os.path.join(os.getcwd(),r'colourised')
+   if not os.path.exists(newdir):
+      os.makedirs(newdir)
+   for file in os.listdir():
+       try:
+             print("using: "+file)
+             r = requests.post(
+                "https://api.deepai.org/api/colorizer",
+                files={'image': open(file, 'rb'),},
+                headers={'api-key': yourapikey}
+            )
+             outlink = r.json()
+             newfilename = os.path.splitext(os.path.basename(file))[0]+"_colourised.jpg"
+             newfilepath = os.path.join(newdir,newfilename)
+             originalfilepath = os.path.abspath(file)
+             urllib.request.urlretrieve(outlink["output_url"],newfilepath)
+             print("created new file "+newfilename+"\n")
+             if (highrescolouroverlay):
+                bcwh.main(originalfilepath,newfilepath,True)
+       except:
+           print("skipping "+file+", not valid image.\n")
+else:
+      print("you are using the default key, please consider signing up at deepai.org :)\n")
+      os.system("start \"\" https://deepai.org/dashboard/profile")
 
 input("press enter to exit")
