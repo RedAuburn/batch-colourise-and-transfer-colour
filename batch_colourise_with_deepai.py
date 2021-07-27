ccaf602ccbcc655dffeb1bade27e4dea7a67c29e
Harry Bond <smashedchant@gmail.com>
2021-07-27 20:01:16 +0100

    add timer to diagnose speed problems

--- batch_colourise_with_deepai.py
+++ batch_colourise_with_deepai.py
@@ -1,11 +1,11 @@
 import urllib.request
-import requests, json, sys, os, webbrowser
+import requests, json, sys, os, webbrowser, time
 import blend_colour_with_highres as bcwh
+from time import perf_counter
 
 highrescolouroverlay = True
 seperatehiresfile = False #not working?
 localscaleb4up = False #not used yet
-yourapikey = ""
 
 with open(os.path.realpath("APIKey.txt")) as f:
     yourapikey = f.read().split()[0]
@@ -17,9 +17,9 @@ def main():
          os.makedirs(newdir)
       for file in os.listdir("./input"):
           try:
-              file_extension = str.lower(os.path.splitext(file)[1])
-              if(file_extension in [".jpeg", ".jpg", ".png", ".bmp"]):
+              if(str.lower(os.path.splitext(file)[1]) in [".jpeg", ".jpg", ".png", ".bmp"]):
                     print("using: " + file)
+                    c1 = time.perf_counter()
                     r = requests.post(
                        "https://api.deepai.org/api/colorizer",
                        files={'image': open(os.path.join("./input",file), 'rb'),},
@@ -27,14 +27,19 @@ def main():
                     )
                     print(r.json())
                     outlink = r.json()
-                    name= os.path.splitext(os.path.basename(file))
+                    count1 = ("time to recieve API response: "+str(round((time.perf_counter()-c1),2))+"s")
+                    c1 = time.perf_counter()
+                    name = os.path.splitext(os.path.basename(file))
                     newfilename = name[0]+"_colourised"+name[1]
                     newfilepath = os.path.join(newdir,newfilename)
                     originalfilepath = os.path.join("./input",file)
                     urllib.request.urlretrieve(outlink["output_url"],newfilepath)
-                    print("created new file "+newfilename+"\n")
+                    count2 = ("time to generate new path: "+str(round((time.perf_counter()-c1),2))+"s")
+                    c1 = time.perf_counter()
                     if (highrescolouroverlay):
                        bcwh.main(originalfilepath,newfilepath,True,seperatehiresfile)
+                       count3 = ("time to blend colour with highres: "+str(round((time.perf_counter()-c1),2))+"s")
+                    print(count1+"\n"+count2+"\n"+count3)
                     webbrowser.open(os.path.realpath(os.path.split(newfilepath)[0]))
               else:
                   print("skipping " + file + ", not a valid image.\n")
@@ -42,6 +47,8 @@ def main():
           	print("you need an API key, please consider sign up for one at deepai.org :)\n")
           	webbrowser.open("https://deepai.org/dashboard/profile")
 
+
 if __name__ == "__main__":
     main()
+
     input("press enter to exit")
